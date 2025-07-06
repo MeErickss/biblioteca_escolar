063b1486a02276056749b96ef63fadae21bc8732
stelinha <oompaloompas007@gmail.com>
2025-07-06 11:57:35 -0300

    .
    .

--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -7,17 +7,20 @@ import { NavLink } from "react-router-dom";
 export function Header() {
   return (
     <div className="absolute z-50">
-      {/* Cabeçalho/menu que aparece em todas as páginas */}
       <header className='flex flex-wrap justify-between px-16 py-4 w-[99vw] h-1/6 bg-neutral-300'>
         <div className="flex flex-row gap-12">
-            <img width={160} src={Logo} alt="Logo"/>
-            <img width={360} src={UTFPR} alt="UTFPR" />
+          <img width={160} src={Logo} alt="Logo" />
+          <img width={360} src={UTFPR} alt="UTFPR" />
         </div>
+
         <NavLink to="/login" className="cursor-pointer">
-          <img src={User} alt="User" />
-        </NavLink> 
+          <div className="bg-yellow-400 w-12 h-12 flex items-center justify-center rounded-md shadow-md">
+            <img src={User} alt="User" className="w-8 h-8" />
+          </div>
+        </NavLink>
       </header>
-    <Reinaldo/>
+
+      <Reinaldo />
     </div>
   );
 }
