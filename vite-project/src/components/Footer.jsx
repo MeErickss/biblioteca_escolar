9b03e942b89b4fb0e22d8e8674c4798105e8078d
stelinha <oompaloompas007@gmail.com>
2025-07-07 09:02:15 -0300

    footer

--- vite-project/src/components/Footer.jsx
+++ vite-project/src/components/Footer.jsx
@@ -1,16 +1,23 @@
-import wLogo from ".././images/wLogo.svg"
+import wLogo from ".././images/wLogo.svg";
 
 export function Footer() {
   return (
-    <div className="absolute w-full h-96 bg-neutral-500 text-white py-30 px-12">
-        <div className="text-end">Telefone</div>
-        <footer className='grid grid-rows-1 grid-cols-4'>
-            <img width={310} src={wLogo} alt="whiteLogo" />
-            <div>a</div>
-            <div>b</div>
-            <div>c</div>
-        </footer>
-      <div className="text-center">a</div>
-    </div>
+    <footer className="bg-neutral-500 text-white px-16 py-12">
+
+      <div className="flex justify-between flex-wrap gap-12">
+        <div className="flex flex-col max-w-sm gap-2">
+          <img src={wLogo} alt="Logo UTFPR" className="w-60" />
+
+          <p className="text-sm">Via Rosalina Maria dos Santos, 1233 - Vila Carolo</p>
+          <p className="text-sm">87301-899 Campo Mourão PR Brasil</p>
+          <p className="text-sm">+55 (44) 3518-1400</p>
+        </div>
+      </div>
+
+
+      <div className="text-center mt-4 text-xs">
+        © {new Date().getFullYear()} UTFPR. Todos os direitos reservados.
+      </div>
+    </footer>
   );
 }
