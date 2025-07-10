b73a4c04629460aa6a274e147ba74247d163b04b
stelinha <oompaloompas007@gmail.com>
2025-07-09 21:20:14 -0300

    updade footer
    voltar ao topo

--- vite-project/src/components/Footer.jsx
+++ vite-project/src/components/Footer.jsx
@@ -1,21 +1,33 @@
 import wLogo from ".././images/wLogo.svg";
+import { UpOutlined } from '@ant-design/icons';
 
 export function Footer() {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
-    <footer className="bg-neutral-500 text-white px-16 py-12">
+    <footer className="relative bg-neutral-500 text-white px-16 py-12">
 
       <div className="flex justify-between flex-wrap gap-12">
         <div className="flex flex-col max-w-sm gap-2">
           <img src={wLogo} alt="Logo UTFPR" className="w-60" />
-
           <p className="text-sm">Via Rosalina Maria dos Santos, 1233 - Vila Carolo</p>
           <p className="text-sm">87301-899 Campo Mourão PR Brasil</p>
           <p className="text-sm">+55 (44) 3518-1400</p>
         </div>
       </div>
 
+      {/* Botão "Voltar ao topo" */}
+      <button
+        onClick={scrollToTop}
+        className="absolute right-8 top-8 bg-gray text-neutral-700 hover:bg-gray-400 p-6 rounded-full shadow-md transition cursor-pointer"
+        aria-label="Voltar ao topo"
+      >
+        <UpOutlined />
+      </button>
 
-      <div className="text-center mt-4 text-xs">
+      <div className="text-center mt-12 text-xs">
         © {new Date().getFullYear()} UTFPR. Todos os direitos reservados.
       </div>
     </footer>
